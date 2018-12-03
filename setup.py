63ef1575b3a0651e8a181cbe01dfaf9a9e497723
Duo Wang <wangd3109@gmail.com>
2018-12-03 22:32:14 +0100

    version 0.0.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='wdgameoflife',
-        version='0.0.1',
+        version='0.0.3',
         description='This is a project for the course Advanced Scientific Programming in Python - Uppsala University',
         url='https://github.com/wangd3109/wdgameoflife',
         author='Duo Wang',
